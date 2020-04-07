6d782e7f91af6dc151a6b01a4865df5bca962b18
Marlo <marlovigor26@gmail.com>
2020-04-07 01:14:06 -0400

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import Options from './Components/Options';
-import Features from './Features'
-import Hardware from './Components/Hardware';
-import Total from './Components/Total';
-import Summary from './Components/Summary';
-import Feature from './Components/Feature';
-
 
 // Normalizes string as a slug - a string that is safe to use
 // in both URLs and html attributes
 import slugify from 'slugify';
 
 import './App.css';
-
+import Header from './Header.js'
+import Main from './Main/Main'
 // This object will allow us to
 // easily convert numbers into US dollar values
 const USCurrencyFormat = new Intl.NumberFormat('en-US', {
@@ -39,9 +33,9 @@ class App extends Component {
         name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
         cost: 1500
       }
-    },
-  }
-  // UPDATE CART
+    }
+  };
+
   updateFeature = (feature, newValue) => {
     const selected = Object.assign({}, this.state.selected);
     selected[feature] = newValue;
@@ -50,37 +44,35 @@ class App extends Component {
     });
   };
 
-  //RENDER CART
   render() {
     const features = Object.keys(this.props.features).map((feature, idx) => {
       const featureHash = feature + '-' + idx;
       const options = this.props.features[feature].map(item => {
         const itemHash = slugify(JSON.stringify(item));
         return (
-        <>
-          <Options
-          key={itemHash}
-          id={itemHash}
-          name={slugify(feature)}
-          checked={item.name === this.state.selected[feature].name}
-          onChange={e => this.updateFeature(feature, item)}
-          /> 
-          
-          <label htmlFor={itemHash} className="feature__label">
-          {item.name} ({USCurrencyFormat.format(item.cost)})
-          </label>
-        </>
+          <div key={itemHash} className="feature__item">
+            <input
+              type="radio"
+              id={itemHash}
+              className="feature__option"
+              name={slugify(feature)}
+              checked={item.name === this.state.selected[feature].name}
+              onChange={e => this.updateFeature(feature, item)}
+            />
+            <label htmlFor={itemHash} className="feature__label">
+              {item.name} ({USCurrencyFormat.format(item.cost)})
+            </label>
+          </div>
         );
       });
 
       return (
-
-        <Hardware 
-        title={feature}
-        featureHash={featureHash}
-        options = {options}     
-        />
-
+        <fieldset className="feature" key={featureHash}>
+          <legend className="feature__name">
+            <h3>{feature}</h3>
+          </legend>
+          {options}
+        </fieldset>
       );
     });
 
@@ -106,31 +98,15 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header>
-          <h1>ELF Computing | Laptops</h1>
-        </header>
-        <main>
-          <form className="main__form">
-            <h2>Customize your laptop</h2>
-            <Feature
-            features={features}
-            />
-          </form>
-          <section className="main__summary">
-            <h2>Your cart</h2>
-            <Summary
-            summary={summary}
-            />
-            <div className="summary__total">
-              <div className="summary__total__label">Total</div>
-              <div className="summary__total__value">
-                <Total 
-                Total={total}
-                />
-              </div>
-            </div>
-          </section>
-        </main>
+        <Header />
+        <Main 
+          updateFeature ={this.updateFeature} 
+          features={this.props.features} 
+          summary={summary}
+          Features={features}
+          total={total}
+          USCurrencyFormat={USCurrencyFormat}
+        />
       </div>
     );
   }
